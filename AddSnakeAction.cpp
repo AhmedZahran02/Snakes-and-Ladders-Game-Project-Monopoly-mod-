220f05cf70cf2bb34d40c8ea1ea324074a66e9c1
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2021-12-31 09:02:48 +0200

    Snake and AddSnake

---
+++ AddSnakeAction.cpp
@@ -0,0 +1,66 @@
+#include "AddSnakeAction.h"
+
+#include "Input.h"
+#include "Output.h"
+#include "Snake.h"
+
+
+AddSnakeAction::AddSnakeAction(ApplicationManager* pApp):Action(pApp) {
+
+}
+
+void AddSnakeAction::Execute() {
+	ReadActionParameters();
+
+	Snake* pSnake = new Snake(startPos,endPos);
+	Grid* pGrid = pManager->GetGrid(); // We get a pointer to the Grid from the ApplicationManager
+
+	// Add the card object to the GameObject of its Cell:
+	bool added = pGrid->AddObjectToCell(pSnake);
+
+	// if the GameObject cannot be added
+	if (!added)
+	{
+		// Print an appropriate message
+		pGrid->PrintErrorMessage("Error: Cell already has an object ! Click to continue ...");
+	}
+}
+void AddSnakeAction::ReadActionParameters() {
+	// Get a Pointer to the Input / Output Interfaces
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+
+	// Read the startPos parameter
+	pOut->PrintMessage("New Snake: Click on its Start Cell ...");
+	startPos = pIn->GetCellClicked();
+
+	// Read the endPos parameter
+	pOut->PrintMessage("New Snake: Click on its End Cell ...");
+	endPos = pIn->GetCellClicked();
+	//Doing the validations for the snake
+	bool Valid = true;
+	if ((startPos.GetCellNum() == 1)) {
+		pOut->PrintMessage("Cannot put any Game object on the first cell");
+		Valid = false;
+	}
+	else if (startPos.HCell() != endPos.HCell()) {
+		pOut->PrintMessage("The Snake can be vertical only!");
+		Valid = false;
+	}
+	else if (startPos.VCell() >= endPos.VCell()) {
+		pOut->PrintMessage("The Snake can be positioned to down only !");
+		Valid = false;
+	}
+	if (!Valid) {
+		endPos.SetHCell(0);
+		endPos.SetVCell(0);
+		startPos.SetVCell(0);
+		endPos.SetHCell(0);
+	}
+
+
+}
+AddSnakeAction::~AddSnakeAction() {
+
+}
