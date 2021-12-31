ab93441a65761849e30033c61079cff3720b3ed2
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2021-12-31 14:01:50 +0200

    Delete Card Done

---
+++ DeleteGameObjectAction.cpp
@@ -0,0 +1,37 @@
+#include "DeleteGameObjectAction.h"
+#include "GameObject.h"
+
+DeleteGameObjectAction::DeleteGameObjectAction(ApplicationManager* pApp) :Action(pApp) {
+
+}
+DeleteGameObjectAction::~DeleteGameObjectAction() {
+
+}
+void DeleteGameObjectAction::Execute() {
+	ReadActionParameters();
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+	bool deleted = pGrid->RemoveObjectFromCell(GameObjectPosition);
+	if (!deleted) {
+		pOut->PrintMessage("There is no GameObject here Click to continue");
+	}
+	else {
+		pOut->PrintMessage("GameObject deleted successfully Click to continue");
+	}
+	int x, y;
+	pIn->GetPointClicked(x, y);
+	// 5- Clear status bar
+	pOut->ClearStatusBar();
+	//update interface
+	pGrid->UpdateInterface();
+}
+void DeleteGameObjectAction::ReadActionParameters() {
+	// Get a Pointer to the Input / Output Interfaces
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+	pOut->PrintMessage("Click on the cell with the game object to delete");
+	GameObjectPosition = pIn->GetCellClicked();
+
+}
