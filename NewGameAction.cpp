d5c962464a19686e1562be04133777fe3d4cf8d1
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-01-06 19:31:01 +0200

    new game error

--- NewGameAction.cpp
+++ NewGameAction.cpp
@@ -1,6 +1,9 @@
 #include "NewGameAction.h"
 #include "Grid.h"
 #include "Player.h"
+#include "CardNine.h"
+#include "CardTen.h"
+#include "CardEleven.h"
 
 NewGameAction::NewGameAction(ApplicationManager* pApp)
 	: Action(pApp)
@@ -18,6 +21,11 @@ void NewGameAction::Execute()
 	Grid* pGrid = pManager->GetGrid();
 	pGrid->SetEndGame(0);
 	CellPosition start(8, 0);
+
+	CardNine::setowner(NULL);
+	CardTen::setowner(NULL);
+	CardEleven::setowner(NULL);
+
 	for (int i = 0; i < 4; i++) {
 		Player* pPlayer = pGrid->GetCurrentPlayer();
 		pGrid->UpdatePlayerCell(pPlayer, start);
